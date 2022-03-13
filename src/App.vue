1f5e17afbac87bcc43cb26f91b26e5acd4544da4
ArisLittle <zglit@qq.com>
2022-03-13 15:57:42 +0800

    chore: init.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <main-game />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import MainGame from './components/MainGame.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
+    MainGame
+  }
 });
 </script>
 
