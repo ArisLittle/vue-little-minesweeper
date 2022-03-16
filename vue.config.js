3b2555476b35462ddc4c743ff21e30ff977118c6
ArisLittle <zglit@qq.com>
2022-03-16 23:19:06 +0800

    chore: change publicPath.

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: 'docs'
+  outputDir: 'docs',
+  publicPath: './'
 });
